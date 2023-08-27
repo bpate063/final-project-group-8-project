edf8e2b84784cfdd7898518b82f1ee788b6d937e
snikt001 <snikt001@ucr.edu>
2023-08-26 19:23:16 -0700

    movie.cpp base

--- movie.cpp
+++ movie.cpp
@@ -2,3 +2,23 @@
 #include <iostream>
 
 using namespace std;
+
+Movie::Movie() {
+
+}
+
+string Movie::getTitle() {
+
+}
+
+string Movie::getRating() {
+
+}
+
+string Movie::getLeadActor() {
+
+}
+
+string Movie::getDirector() {
+
+}
